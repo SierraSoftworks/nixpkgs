0ed0fe328e1099dd2a15eefd93cf57e3b9a46cc1
Benjamin Pannell <benjamin@pannell.dev>
2022-11-09 14:15:19 +0000

    fix: Resolve issues with running nix-build release.nix

--- release.nix
+++ release.nix
@@ -1,20 +1,17 @@
 {
-    nixpkgsSrc ? <nixpkgs>,
-    defaultTargets ? [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
+    nixpkgsSrc ? <nixpkgs>
 }:
 
 let
-    lib = (import nixpkgsSrc { }).lib;
-    builder = path: target:
-        lib.getAttrFromPath path (import nixpkgsSrc {
-            overlays = [
-                import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz)
-                import ./default.nix
-            ];
-        });
-    build = path: { targets ? defaultTargets }:
-        lib.genAttrs targets builder;
-in {
-    git-tool = build ["git-tool"];
-    grey = build ["grey"];
-}
+    nixpkgs = import nixpkgsSrc {
+        overlays = [
+            (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+            (import ./default.nix)
+        ];
+    };
+
+    jobs = {
+        git-tool = nixpkgs.git-tool;
+        grey = nixpkgs.grey;
+    };
+in jobs
